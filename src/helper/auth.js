e3f38c9b47b11177d4e0296ca7e620d5e4535ea0
msuryasyahruli <msuryasyahruli12@gmail.com>
2024-05-20 17:02:58 +0700

    update helper auth

--- src/helper/auth.js
+++ src/helper/auth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 const generateToken = (payload) => {
   const verifyOpts = {
-    expiresIn: "1h",
+    expiresIn: "1day",
   };
   const token = jwt.sign(payload, process.env.SECRETE_KEY_JWT, verifyOpts);
   return token;
